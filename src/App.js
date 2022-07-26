39ac6e66da03aadbe620e6e1fe2aef44fac2a616
ABRUIZ25 <89878760+ABRUIZ25@users.noreply.github.com>
2022-07-26 15:31:53 -0400

    user is being seen on the page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
 
   const [clientMessage, setClientMessage] = useState('')
   const [serverMessage, setServerMessage] = useState('')
+  const [userList, setUserList] = useState([])
 
   const urlEndpoint = process.env.REACT_APP_URL_ENDPOINT;
 
@@ -25,14 +26,35 @@ function App() {
     })
     const responseJSON = await response.json()
     setServerMessage(responseJSON)
-
   }
+  useEffect(() => {
+    const fetchUser = async () => {
+
+      const response = await fetch(`${urlEndpoint}/get-users`, {
+        method: 'GET',
+        headers: {
+          accept: 'application.json',
+          'Content-Type': 'application/json'
+        },
+
+      })
+
+
+
+
+      const json = await response.json()
+      console.log(json)
+      setUserList(json)
+      return json
+    };
+    fetchUser()
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <div>hello</div>
         <Routes>
-          <Route index element={<HomePage clientMessage={clientMessage} setClientMessage={setClientMessage} serverMessage={serverMessage} sendReceiveMessage={sendReceiveMessage} />} />
+          <Route index element={<HomePage clientMessage={clientMessage} setClientMessage={setClientMessage} serverMessage={serverMessage} sendReceiveMessage={sendReceiveMessage} userList={userList} />} />
         </Routes>
 
       </header>
