c29fb9d951d36aad14d729cfaa5bd09ab7ed6cec
Sarah Lopez <slopez323@gmail.com>
2022-07-26 12:40:43 -0400

    post user data

--- src/App.js
+++ src/App.js
@@ -1,40 +1,66 @@
-import './App.css';
-import { useState, useEffect } from "react";
+import "./App.css";
+import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
-
+import PostUser from "./Pages/PostUser";
 
 function App() {
-
-  const [clientMessage, setClientMessage] = useState('')
-  const [serverMessage, setServerMessage] = useState('')
+  const [clientMessage, setClientMessage] = useState("");
+  const [serverMessage, setServerMessage] = useState("");
 
   const urlEndpoint = process.env.REACT_APP_URL_ENDPOINT;
 
-
   const sendReceiveMessage = async () => {
-    const url = `${urlEndpoint}/post-message`
+    const url = `${urlEndpoint}/post-message`;
     const response = await fetch(url, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        accept: 'application.json',
-        'Content-Type': 'application/json'
+        accept: "application.json",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ clientMessage }),
-      cache: 'default'
-    })
-    const responseJSON = await response.json()
-    setServerMessage(responseJSON)
+      cache: "default",
+    });
+    const responseJSON = await response.json();
+    setServerMessage(responseJSON);
+  };
 
-  }
+  const postUserData = async (userData) => {
+    const url = `${urlEndpoint}/create-user`;
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        accept: "application.json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userData }),
+      cache: "default",
+    });
+    const responseJSON = await response.json();
+    setServerMessage(responseJSON);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <div>hello</div>
         <Routes>
-          <Route index element={<HomePage clientMessage={clientMessage} setClientMessage={setClientMessage} serverMessage={serverMessage} sendReceiveMessage={sendReceiveMessage} />} />
+          <Route
+            index
+            element={
+              <HomePage
+                clientMessage={clientMessage}
+                setClientMessage={setClientMessage}
+                serverMessage={serverMessage}
+                sendReceiveMessage={sendReceiveMessage}
+              />
+            }
+          />
+          <Route
+            path="/post-user"
+            element={<PostUser postUserData={postUserData} />}
+          />
         </Routes>
-
       </header>
     </div>
   );
